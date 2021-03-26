2844437fcae14f0f6d7a0ccd4759d5b8614f1c84
Dmitry <dmitry.balduev@gmail.com>
2021-03-25 22:51:10 -0400

    added list with all countries and after selecting one of them, it will print info on this country

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let countryInput = $("#country-input");
-
+let cityForWeather = "";
+let arrayCountries = [];
+let selectCountries = $("#select-countries");
 
 $("#submit-button").on("click", renderSearch);
 
 function renderSearch(event){
 
     event.preventDefault();
-    let country = countryInput.val();
+    let country = "";
+    if(selectCountries[0].value != null){
+        country = selectCountries[0].value;
+    }else{
+        country = countryInput.val();
+    }
     let url = "http://covid-api.mmediagroup.fr/v1/cases?country=" + country;
     fetch(url)
         .then(function (response){
@@ -14,12 +21,37 @@ function renderSearch(event){
                 alert("Invalid country");
             }
             return response.json();
-
         })
         .then(function(data){
             console.log(data);
-            $("#output").text("Confirmed cases: " + data.All.confirmed);
+            $("#country-name").text(country);
+            $("#population").text("Population: " + data.All.population);
+            $("#capital-name").text("Catital city: " + data.All.capital_city);
+            $("#confirmed-cases").text("Confirmed cases: " + data.All.confirmed);
+            $("#deaths").text("Deaths: " + data.All.deaths);
+            $("#recovered").text("Deaths: " + data.All.recovered);
+
         })
     
 }
 
+
+function getListCountry(){
+    let url = "http://covid-api.mmediagroup.fr/v1/cases"
+    
+    fetch(url)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            arrayCountries = Object.keys(data);
+            arrayCountries.splice( arrayCountries.length-2, 2);
+            console.log(arrayCountries);
+            for(let i = 0; i < arrayCountries.length; i++){
+                selectCountries.append("<option value="+arrayCountries[i]+">"+arrayCountries[i]+"</option>")
+            }
+            selectCountries.show();
+        })
+        
+}
+getListCountry();
